ba4fb2859ecf6c20b3d9c7421e465b858dff56c6
rafiqwe <rmlmanik502@gmail.com>
2026-02-12 00:56:19 +0600

    Updated: CLI

--- src/cli.ts
+++ src/cli.ts
@@ -1,18 +1,70 @@
 #!/usr/bin/env node
-import chalk from "chalk";
-import { askSetup } from "./core/prompts.js";
-import { installDeps } from "./core/install.js";
+
+import { injectAuthConfig } from "./core/injectAuthConfig.js";
+import { runSetup } from "./engine/prompts/setup.js";
+import { injectMiddleware } from "./core/injectMiddleware.js";
 import { generateEnv } from "./core/env.js";
-import { injectAuthConfig } from "./core/injectConfig.js";
-import { copyTemplate } from "./utils/copyTemplate.js";
-async function run() {
-  console.log(chalk.cyan("\nAuth Starter Setup\n"));
-  const answers = await askSetup();
-  await copyTemplate();
-  await installDeps();
-  generateEnv();
-  injectAuthConfig(answers);
-  console.log(chalk.green("\nSetup Complete 🚀\n"));
+import { generateUI } from "./generators/ui/index.js";
+import { intro, outro } from "@clack/prompts";
+import { installDeps } from "./core/install.js";
+import { loadProviders } from "./generators/providers/index.js";
+import { generatePrisma } from "./generators/database/prisma/index.js";
+import { prismaDeps } from "./generators/database/prisma/deps.js";
+import { prismaConfig } from "./generators/database/prisma/prismaConfig.js";
+import { injectUserType } from "./core/injectUserType.js";
+import chalk from "chalk";
+// import { detectEnv } from "./engine/detect/env.js";
+
+async function main() {
+  intro("Welcome to Auth Starter");
+  // const env = await detectEnv();
+  const config = await runSetup();
+  const targetDir = process.cwd();
+  const ifCredential = config.providers.filter((p) => p !== "credentials");
+  const ifCredentialProvider = config.providers.filter(
+    (p) => p === "credentials",
+  );
+
+  // inject auth config
+  injectAuthConfig(config);
+  if (ifCredentialProvider) {
+    injectUserType();
+  }
+
+  // if middleware is selected then inject middleware
+  if (config.middleware) {
+    injectMiddleware();
+  }
+
+  // generate env variables
+
+  const envs = ifCredential.flatMap((p) => [
+    `${p.toUpperCase()}_CLIENT_ID`,
+    `${p.toUpperCase()}_CLIENT_SECRET`,
+  ]);
+
+  if (config.ui) {
+    generateUI(targetDir, config.providers);
+  }
+
+  const deps = loadProviders(config.providers).flatMap((p) =>
+    p.getDependencies(),
+  );
+
+  // if database is postgres or mongodb then generate prisma
+  if (config.database === "postgres" || config.database === "mongodb") {
+    generatePrisma(targetDir, config.database);
+    // if database is postgres then generate prisma config
+    if (config.database === "postgres") prismaConfig();
+  }
+
+  // generate env variables
+  generateEnv({ variables: envs });
+
+  // install dependencies
+  await installDeps({ deps: [...deps, ...prismaDeps()], ans: config.database });
+  console.log(chalk.green(".env created"));
+  outro("Auth Starter completed");
 }
 
-run();
+main();
