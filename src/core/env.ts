fa58985202da7d192f176bdc9b47c52e646909b8
rafiqwe <rmlmanik502@gmail.com>
2026-02-12 00:50:26 +0600

    Updated: env core

--- src/core/env.ts
+++ src/core/env.ts
@@ -2,17 +2,15 @@ import fs from "fs";
 import crypto from "crypto";
 import chalk from "chalk";
 
-export function generateEnv() {
+export function generateEnv({ variables }: { variables: string[] }) {
   const secret = crypto.randomBytes(32).toString("hex");
 
   const content = `
 NEXTAUTH_SECRET=${secret}
 NEXTAUTH_URL=http://localhost:3000
-GOOGLE_CLIENT_ID=
-GOOGLE_CLIENT_SECRET=
+DATABASE_URL="postgresql://USER:PASSWORD@localhost:5432/db"
+${variables.map((v) => `${v}=`).join("\n")}
 `;
 
-  fs.writeFileSync(".env.local", content);
-
-  console.log(chalk.green(".env.local created"));
+  fs.writeFileSync(".env", content);
 }
