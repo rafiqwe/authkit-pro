4656e0d28070c809298a60855cb3c339c7400200
rafiqwe <rmlmanik502@gmail.com>
2026-02-12 00:49:36 +0600

    Created: Inject middleware core

---
+++ src/core/injectMiddleware.ts
@@ -0,0 +1,26 @@
+import { write } from "../utils/write.js";
+
+export function injectMiddleware() {
+  const file = `
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
+
+export const config = {
+  matcher: [
+    "/",
+    "/dashboard/:path*",
+    "/settings/:path*",
+    "/profile/:path*",
+    // Add other protected routes here
+  ],
+};
+
+`;
+
+  write(process.cwd(), "middleware.ts", file);
+}
