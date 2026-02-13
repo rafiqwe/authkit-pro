c0ffa97a62fa034aaa83e50fd87837cf2ce292ae
rafiqwe <rmlmanik502@gmail.com>
2026-02-13 17:30:09 +0600

    Created: Session Helper generator

---
+++ src/generators/ui/sessionHelper.ts
@@ -0,0 +1,8 @@
+export function sessionHelper() {
+    return `
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth";
+
+export const getSession = () => getServerSession(authOptions);
+`;
+  }
