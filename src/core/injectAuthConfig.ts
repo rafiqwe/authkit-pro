256ddc268f35d61ed68621a3cd31d7f5086d4edd
rafiqwe <rmlmanik502@gmail.com>
2026-02-14 22:18:16 +0600

    Updated: Inject auth config core

--- src/core/injectAuthConfig.ts
+++ src/core/injectAuthConfig.ts
@@ -1,24 +1,41 @@
-import { AuthCLIConfig } from "../types/config.js";
+import { AuthCLIConfig, Database } from "../types/config.js";
 import { loadProviders } from "../generators/providers/index.js";
 import { injectDynamicRoute } from "./injectDynamicRoute.js";
 import { write } from "../utils/write.js";
 
-export function injectAuthConfig(ans: AuthCLIConfig) {
-  const providers = loadProviders(ans.providers);
+export function injectAuthConfig(ans: AuthCLIConfig, db: Database) {
+  const adapter =
+    ans.database === "postgres"
+      ? "adapter: PrismaAdapter(prisma),"
+      : "adapter: MongoDBAdapter(clientPromise),";
+
+  const importAdapter =
+    ans.database === "postgres"
+      ? 'import { PrismaAdapter } from "@auth/prisma-adapter";'
+      : 'import { MongoDBAdapter } from "@next-auth/mongodb-adapter"\nimport clientPromise from "./mongodb"';
+
+  const prismaImportLogic =
+    ans.providers.includes("credentials") ||
+    ans.database === "mongodb" ||
+    ans.database === "none";
+
+  const PrismaImport = prismaImportLogic
+    ? ""
+    : 'import { prisma } from "@/lib/prisma";';
+
+  const providers = loadProviders(ans.providers, db);
+
   const file = `
 import NextAuth from "next-auth";
+import { NextAuthOptions } from "next-auth";
 ${providers.map((p) => p.getImport()).join("\n")}
-${
-  ans.database === "postgres"
-    ? 'import { PrismaAdapter } from "@auth/prisma-adapter" \n import { NextAuthOptions } from "next-auth";'
-    : ""
-}
+${importAdapter}
+${PrismaImport}
 
 export const authOptions: NextAuthOptions = {
-
-  ${ans.database === "postgres" ? "adapter: PrismaAdapter(prisma)," : ""}
+  ${adapter}
   providers: [
-    ${providers.map((p) => p.getConfig()).join(",")}
+    ${providers.map((p) => p.getConfig()).join(",\n")}
   ],
   session: { strategy: "jwt" },
   pages: { signIn: "/auth/signin" },
@@ -26,7 +43,6 @@ export const authOptions: NextAuthOptions = {
 
 export default NextAuth(authOptions);
 `;
-
   write(process.cwd(), "lib/auth.ts", file);
   injectDynamicRoute();
 }
