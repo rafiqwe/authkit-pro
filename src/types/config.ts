edb1667f09d2e6bc1de9d0785c91fb3f82b6c441
rafiqwe <rmlmanik502@gmail.com>
2026-02-17 23:59:34 +0600

    Updated: Config types

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,12 +1,11 @@
-export type Provider =
-  | "google"
-  | "github"
-  | "facebook"
-  | "credentials";
+export type Provider = "google" | "github" | "facebook" | "credentials";
 
 export type Database = "mongodb" | "postgres" | "none";
 
+export type Engine = "authjs" | "nextauth";
+
 export interface AuthCLIConfig {
+  engine: Engine;
   providers: Provider[];
   middleware: boolean;
   database: Database;
