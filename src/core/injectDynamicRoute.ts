9f5f830e17a3606aa9626171430715a5ab06470b
rafiqwe <rmlmanik502@gmail.com>
2026-02-19 19:51:32 +0600

    Updated: inject Dynamic route core for authjs

--- src/core/injectDynamicRoute.ts
+++ src/core/injectDynamicRoute.ts
@@ -1,27 +1,42 @@
 import fs from "fs";
 import path from "path";
 import { write } from "../utils/write.js";
+import { Engine } from "../types/config.js";
 
-export function injectDynamicRoute() {
+export function injectDynamicRoute({ engine }: { engine: Engine }) {
   // auth route
-  const fileContent = `
+  let fileContent = "";
+  if (engine === "nextauth") {
+    fileContent = `
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
-`;
+  `;
+  } else {
+    fileContent = `
+//app/api/auth/[...authjs]/route.ts
+import { handlers } from "@/lib/auth";
+
+export const { GET, POST } = handlers;
+
+    `;
+  }
 
   // check route
   const checkRouteFile = `
 // app/api/auth/check/route.ts
 import { NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
+${
+  engine === "nextauth"
+    ? `import { getServerSession } from "next-auth";\nimport { authOptions } from "@/lib/auth";`
+    : `import { auth } from "@/lib/auth";`
+}
 
 export async function GET(req: Request) {
-  const session = await getServerSession(authOptions);
+  ${engine === "nextauth" ? "const session = await getServerSession(authOptions);" : "const session = await auth();"}
 
   if (!session?.user) {
     return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
@@ -33,5 +48,9 @@ export async function GET(req: Request) {
 `;
 
   write(process.cwd(), "app/api/auth/check/route.ts", checkRouteFile);
-  write(process.cwd(), "app/api/auth/[...nextauth]/route.ts", fileContent);
+  write(
+    process.cwd(),
+    `app/api/auth/[...${engine === "nextauth" ? "nextauth" : "authjs"}]/route.ts`,
+    fileContent,
+  );
 }
